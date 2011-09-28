3cc009afbb576a71fa580930006c3ccf1ecb403b
Vijay Dev <vijaydev.cse@gmail.com>
2011-09-29 00:10:57 +0530

    bundler exec

---
+++ bundler-exec.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BUNDLED_COMMANDS="${BUNDLED_COMMANDS:-cucumber heroku rackup rails rake rspec ruby shotgun spec spork}"
+
+## Functions
+
+bundler-installed()
+{
+    which bundle > /dev/null 2>&1
+}
+
+within-bundled-project()
+{
+    local dir="$(pwd)"
+    while [ "$(dirname $dir)" != "/" ]; do
+        [ -f "$dir/Gemfile" ] && return
+        dir="$(dirname $dir)"
+    done
+    false
+}
+
+run-with-bundler()
+{
+    local command="$1"
+    shift
+    if bundler-installed && within-bundled-project; then
+        bundle exec $command "$@"
+    else
+        $command "$@"
+    fi
+}
+
+## Main program
+
+for CMD in $BUNDLED_COMMANDS; do
+    alias $CMD="run-with-bundler $CMD"
+done
